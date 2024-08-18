34c57e001ef73e0d169cd602ef1eecbe1506378b
Henrique Kramer <henriquemkramer@gmail.com>
2024-08-18 14:47:09 -0300

    feat: add pets repository

---
+++ src/repositories/pets-repository.ts
@@ -0,0 +1,15 @@
+import { Prisma, Pet } from '@prisma/client'
+
+export interface FindAllParams {
+  city: string
+  age?: string
+  size?: string
+  energy_level?: string
+  environment?: string
+}
+
+export interface PetsRepository {
+  findById(id: string): Promise<Pet | null>
+  findAll(params: FindAllParams): Promise<Pet[]>
+  create(data: Prisma.PetCreateInput): Promise<Pet>
+}
