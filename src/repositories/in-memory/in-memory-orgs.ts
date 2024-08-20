704834c41960a689bdce55054a11389bfcc94bb3
Henrique Kramer <henriquemkramer@gmail.com>
2024-08-20 00:20:23 -0300

    feat: add in-memory-orgs and pets

---
+++ src/repositories/in-memory/in-memory-orgs.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Prisma, Org } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { FindManyNearbyParams, OrgsRepository } from '../orgs-repository'
+import { Decimal } from '@prisma/client/runtime/library'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
+
+export class InMemoryOrgsRepository implements OrgsRepository {
+  public items: Org[] = []
+
+  async findById(id: string) {
+    const org = this.items.find((item) => item.id === id)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
+  async findByEmail(email: string) {
+    const org = this.items.find((item) => item.email === email)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter((item) => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      )
+
+      return distance < 10
+    })
+  }
+
+  async create(data: Prisma.OrgUncheckedCreateInput) {
+    const org = {
+      id: crypto.randomUUID(),
+      ...data,
+      latitude: new Decimal(data.latitude.toString()),
+      longitude: new Decimal(data.longitude.toString()),
+    }
+
+    this.items.push(org)
+
+    return org
+  }
+}
