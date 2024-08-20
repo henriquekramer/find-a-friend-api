2c463d5d52cbdf5f85b1c1675b418bdb184aed0c
Henrique Kramer <henriquemkramer@gmail.com>
2024-08-20 00:00:24 -0300

    feat: add authenticate use-case and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
 import { ZodError } from 'zod'
 import { env } from '@/env'
+import fastifyCookie from '@fastify/cookie'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
---
+++ src/use-cases/authenticate-org.ts
@@ -0,0 +1,38 @@
+import { compare } from 'bcryptjs'
+import { Org } from '@prisma/client'
+import { OrgsRepository } from '@/repositories/orgs-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+interface AuthenticateOrgCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateOrgUseCaseResponse {
+  org: Org
+}
+
+export class AuthenticateOrgUseCase {
+  constructor(private orgsRepository: OrgsRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateOrgCaseRequest): Promise<AuthenticateOrgUseCaseResponse> {
+    const org = await this.orgsRepository.findByEmail(email)
+
+    if (!org) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, org.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      org,
+    }
+  }
+}
