00c8a4c4bcbe172039292ac79dfe16e6cbe71dc4
Henrique Kramer <henriquemkramer@gmail.com>
2024-08-19 21:34:45 -0300

    feat: create-pet use-case

---
+++ src/use-cases/create-pet.ts
@@ -0,0 +1,62 @@
+import { OrgsRepository } from '@/repositories/orgs-repository'
+import { PetsRepository } from '@/repositories/pets-repository'
+import {
+  Pet,
+  PetAge,
+  PetEnergyLevel,
+  PetEnvironment,
+  PetSize,
+} from '@prisma/client'
+import { OrgNotFoundError } from './errors/org-not-found-error'
+
+interface CreatePetUseCaseRequest {
+  name: string
+  about: string
+  age: PetAge
+  size: PetSize
+  energy_level: PetEnergyLevel
+  environment: PetEnvironment
+  requirements: string[]
+  org_id: string
+}
+
+interface CreatePetUseCaseResponse {
+  pet: Pet
+}
+
+export class CreatePetUseCase {
+  constructor(
+    private orgsRepository: OrgsRepository,
+    private petsRepository: PetsRepository,
+  ) {}
+
+  async execute({
+    name,
+    about,
+    age,
+    size,
+    energy_level,
+    environment,
+    requirements,
+    org_id,
+  }: CreatePetUseCaseRequest): Promise<CreatePetUseCaseResponse> {
+    const org = await this.orgsRepository.findById(org_id)
+
+    if (!org) {
+      throw new OrgNotFoundError()
+    }
+
+    const pet = await this.petsRepository.create({
+      name,
+      about,
+      age,
+      size,
+      energy_level,
+      environment,
+      requirements,
+      org_id,
+    })
+
+    return { pet }
+  }
+}
